64049c4496f79505d064ab686c76f38c52297913
James Penny <james_penny@hotmail.co.uk>
2023-02-03 20:48:22 +0000

    Initial Commit

---
+++ ExerciseFinder/ExerciseFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExerciseFinderApp.swift
+//  ExerciseFinder
+//
+//  Created by James Penny on 03/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ExerciseFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
